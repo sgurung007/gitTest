49a3b534745a7c2a7a7122660507dcc18109ecd5
Suraj Gurung <sgrg007@gmail.com>
2020-05-22 20:30:52 -0500

    add function in

--- src/com/gitPractice/Main.java
+++ src/com/gitPractice/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("this is my first change");
     }
+    public void displayName(){
+        System.out.println("myName is Suraj Gurung");
+    }
+
 }
